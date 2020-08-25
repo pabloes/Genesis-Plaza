6e4711046e3edffa0b8710ef3d2bb00af009e68c
Pablo Estornut <estornut@gmail.com>
2020-08-25 22:59:45 +0200

    sammich: tweaking position and frame

--- src/game.ts
+++ src/game.ts
@@ -129,24 +129,26 @@ let forest2 = new AmbientSound(
 const landOwnerData = {
   host_data: `{
     "sammich":{
-      "position":{"x":${9*16-11.1},"y":1,"z":${9*16+8}},
+      "position":{"x":${9*16-11.1},"y":1.4,"z":${9*16+8}},
       "rotation":{"x":0,"y":270,"z":0},
-      "scale":{"x":1.2, "y":1.2, "z":1.2},     
+      "scale":{"x":1.2, "y":1.05, "z":1},     
       "hideBoard": false,
       "hideAd": true,
       "gameID": "-95,-87",
-      "soundDistance": 12,
+      "soundDistance": 5,
       "showScenario": false,
       "hideFrame": true,
       "showJoinVoice": false,
       "voiceChannel": "dcl-sammich-game",
-      "serverWs": "wss://foo.mana-fever.fun",
-      "serverHttp": "https://foo.mana-fever.fun"
+      "serverWs": "ws://localhost:2567",
+      "serverHttp": "http://localhost:2567"
     }
  }`
 };
 const sammichFrame = new Entity();
-sammichFrame.addComponent(new GLTFShape("models/sammich-screen.glb"));
+const sammichFrameShape = new GLTFShape("models/sammich-screen.glb");
+sammichFrameShape.isPointerBlocker = false;
+sammichFrame.addComponent(sammichFrameShape);
 sammichFrame.addComponent(new Transform({
   position:new Vector3(9*16-11, 0.5, 9*16+8),
   scale: new Vector3 (1.2,1.2,1.2),
